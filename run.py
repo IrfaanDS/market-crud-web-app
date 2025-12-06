a5d38a227eda91a928ca1ceacbc0b3ec99937a80
IrfaanDS <irfaanexe@gmail.com>
2025-12-06 21:25:28 +0500

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from market import app
 
-#Checks if the run.py file has executed directly and not imported
+# Checks if the run.py file has executed directly and not imported
 if __name__ == '__main__':
-    app.run(debug=True)
+    # CRITICAL FIX: Ensure 'host=0.0.0.0' is present
+    app.run(host='0.0.0.0', port=5000, debug=False)
